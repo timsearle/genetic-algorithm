f501d651936c7475f47f9a26cfc016f5eb201531
Tim Searle <timwebuk@gmail.com>
2015-04-12 20:18:26 +0100

    Initial Commit

---
+++ GeneticAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GeneticAlgorithm
+//
+//  Created by Tim Searle on 12/04/2015.
+//  Copyright (c) 2015 Tim Searle. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
